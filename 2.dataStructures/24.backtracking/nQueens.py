8724e89440962c6c29193d16f8dcc313857b12a9
lokeshvelayudham <lokesh.beyondx@gmail.com>
2023-05-30 14:24:13 +0530

    1

--- 2.dataStructures/24.backtracking/nQueens.py
+++ 2.dataStructures/24.backtracking/nQueens.py
@@ -1,4 +1,45 @@
+#code is contributed by Lokesh Poluru Velayudham
+
+def isSafe(row,col,board,n):
+    # vertical direction
+    i = row -1
+    while i >=0:
+        if board[i][col] == 1:
+            return False
+        i -= 1
+
+    i = row-1
+    j = col-1
+
+    while i>=0 and j>=0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+    i = row-1
+    j = col+1
+    while i>=0 and j<n:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+    return True
+
+
 def printPathHelper(row,n,board):
+    if row == n:
+        for i in range(n):
+            for j in range(n):
+                print(board[i][j],end=" ")
+        print()
+        return
+    for col in range(n):
+        if isSafe(row,col,board,n):
+            board[row][col] = 1
+            printPathHelper(row+1,n,board)
+            board[row][col] = 0
+
+    return
     
 
 def printPaths(n):
