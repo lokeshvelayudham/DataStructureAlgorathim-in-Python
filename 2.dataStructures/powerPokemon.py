a3df03b824b8bf48fdf03c47b8523d3ad1820fbb
lokeshvelayudham <lokesh.beyondx@gmail.com>
2023-06-14 18:28:07 +0530

    1

--- 2.dataStructures/powerPokemon.py
+++ 2.dataStructures/powerPokemon.py
@@ -1,14 +1,29 @@
+import math
+
+def count_power_pokemon_couples(power):
+    count = 0
+    power_counts = {}
+
+    for p in power:
+        if p in power_counts:
+            power_counts[p] += 1
+        else:
+            power_counts[p] = 1
+
+    for p in power:
+        for i in range(1, 23):
+            q = int(math.pow(2, i) - p)
+            if q in power_counts and power_counts[q] > 0:
+                power_counts[q] -= 1
+                count += 1
+                break
+
+    return count % (10**9 + 7)
+
+
 N = int(input())
 power = list(map(int, input().split()))
 
-MOD = int(1e9) + 7
-count = 0
-frequency = {}
 
-for p in power:
-    complement = (1 << (p.bit_length())) - p
-    if complement in frequency:
-        count += frequency[complement]
-    frequency[p] = frequency.get(p, 0) + 1
-
-print(count % MOD)
+result = count_power_pokemon_couples(power)
+print(result)
